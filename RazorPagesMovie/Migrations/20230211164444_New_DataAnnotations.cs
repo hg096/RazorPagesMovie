d306888af8e13cdb35c2ef98c3e213722102fceb
hg096 <hg_96@naver.com>
2023-02-12 01:54:23 +0900

    유효성까지 완료

---
+++ RazorPagesMovie/Migrations/20230211164444_New_DataAnnotations.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RazorPagesMovie.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewDataAnnotations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Movie",
+                type: "nvarchar(60)",
+                maxLength: 60,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(16)",
+                oldMaxLength: 16);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Movie",
+                type: "nvarchar(16)",
+                maxLength: 16,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(60)",
+                oldMaxLength: 60);
+        }
+    }
+}
--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -8,13 +8,29 @@ namespace RazorPagesMovie.Models;
 public class Movie
 {
     public int Id { get; set; }
-    public string? Title { get; set; } // string 다음의 물음표는 속성이 null 허용임을 나타냄
 
-    [Display(Name = "Release Date")]
+    [StringLength(60, MinimumLength = 3, ErrorMessage = "제목을 3자이상 60자 이하로 입력해주세요")]
+    [Required (ErrorMessage = "제목을 입력해주세요")]
+    // public string? Title { get; set; } // string 다음의 물음표는 속성이 null 허용임을 나타냄
+    public string Title { get; set; } = string.Empty;
+
+    // [Display(Name = "Release Date")]
     [DataType(DataType.Date)]
     public DateTime ReleaseDate { get; set; }
-    public string? Genre { get; set; }
 
+    [Range(1, 100)]
+    [DataType(DataType.Currency)]
     [Column(TypeName = "decimal(18, 2)")]
     public decimal Price { get; set; }
+
+    [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+    [Required]
+    [StringLength(30)]
+    public string Genre { get; set; } = string.Empty;
+
+    [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+    [StringLength(5)]
+    [Required]
+    public string Rating { get; set; } = string.Empty;
+
 }
